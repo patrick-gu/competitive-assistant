e974759fff2045d2266747a5ef8c00d81c8cc3af
Patrick Gu <patrick@patrickgu.ca>
2023-01-15 15:40:08 -0800

    Automatically open notebook test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,7 +5,7 @@ import * as vscode from "vscode";
 suite("Extension Test Suite", () => {
   vscode.window.showInformationMessage("Start all tests.");
 
-  test("Open", async () => {
+  test("Automatically open notebook", async () => {
     await vscode.commands.executeCommand(
       "vscode.openFolder",
       vscode.Uri.file(path.join(__dirname, "../../../example"))
@@ -13,6 +13,13 @@ suite("Extension Test Suite", () => {
     await vscode.commands.executeCommand(
       "vscode.open",
       vscode.Uri.file(path.join(__dirname, "../../../example/double.py"))
+    );
+    const notebookDocument = await new Promise<vscode.NotebookDocument>(
+      (resolve) => {
+        vscode.workspace.onDidOpenNotebookDocument((e) => {
+          resolve(e);
+        });
+      }
     );
   });
 });
